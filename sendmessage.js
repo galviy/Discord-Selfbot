925f46b12634e9afea665a3abf32dd39fab5d61c
Galvins <68190778+Galvin0705@users.noreply.github.com>
2021-11-13 22:38:56 +0700

    Update sendmessage.js

--- sendmessage.js
+++ sendmessage.js
@@ -34,38 +34,41 @@ async function Message(message) {
             }
         })
     } catch (e) {
-		console.log(e)
+        console.log(e)
     }
 }
 
-async function channel(name){
-	try {
-		let channels = {"name": name}
-		fetch("https://discord.com/api/v9/guilds/" + guildid + "/channels", {
-                method: "POST",
-                body: JSON.stringify(channels),
-                headers: {
-                    "Host": "discord.com",
-                    "user-agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Mobile Safari/537.36",
-                    "Content-Type": "application/json; charset=UTF-8",
-					"content-length": "999",
-                    "authorization": config.token
-                }
-            }).then(resp => resp.json()).then(response => {
-				if(response.code == 50013){
-					console.log(response.message)
-				} else if(response.global == true){
-					console.log("Discord cooldown (rate limit): " + response.retry_after)
-				} else {
-					console.log("Created channel: " + response.name + "\nChannel ID: " + response.id)
-				} 
-            }).catch(err => console.log("[Fetch] ECONNRESET"))
-        } catch (e) {
-			console.log(e)
+async function channel(name) {
+    try {
+        let channels = {
+            "name": name
         }
-	
+        fetch("https://discord.com/api/v9/guilds/" + guildid + "/channels", {
+            method: "POST",
+            body: JSON.stringify(channels),
+            headers: {
+                "Host": "discord.com",
+                "user-agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Mobile Safari/537.36",
+                "Content-Type": "application/json; charset=UTF-8",
+                "content-length": "999",
+                "authorization": config.token
+            }
+        }).then(resp => resp.json()).then(response => {
+            if (response.code == 50013) {
+                console.log(response.message)
+            } else if (response.global == true) {
+                console.log("Discord cooldown (rate limit): " + response.retry_after)
+            } else {
+                console.log("Created channel: " + response.name + "\nChannel ID: " + response.id)
+            }
+        }).catch(err => console.log("[Fetch] ECONNRESET"))
+    } catch (e) {
+        console.log(e)
+    }
+
 }
 
 module.exports = {
     Message: Message,
+    channel: name,
 }
